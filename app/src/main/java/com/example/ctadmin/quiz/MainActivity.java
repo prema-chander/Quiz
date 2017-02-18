e83a3bb12465028b09bd9982a9ec28499065247d
ctadmin <cprema@rocketmail.com>
2017-02-18 16:08:13 +0530

    Qizz app :)

--- app/src/main/java/com/example/ctadmin/quiz/MainActivity.java
+++ app/src/main/java/com/example/ctadmin/quiz/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.ctadmin.quiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        b1 = (Button) findViewById(R.id.button);
+
+        // Capture button clicks
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        Main2Activity.class);
+                startActivity(myIntent);
+            }
+        });
+
+    }}
